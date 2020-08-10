5ec33f2ccf3eae929175fd3179af20f3fa56deff
Ella Schwarz <ellaschwarzt@gmail.com>
2020-08-10 17:05:25 +0200

    Handeled different drums

--- project-2-beat-mix-start/public/js/script.js
+++ project-2-beat-mix-start/public/js/script.js
@@ -1,6 +1,46 @@
-// Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+const createEmptyDrumArray = () => new Array(16).fill(false);
 
+let kicks = createEmptyDrumArray();
+let snares = createEmptyDrumArray();
+let hiHats = createEmptyDrumArray();
+let rideCymbals = createEmptyDrumArray();
+
+const getDrumArrayByName = name => {
+  switch (name) {
+    case "kicks":
+      return kicks;
+    case "snares":
+      return snares;
+    case "hiHats":
+      return hiHats;
+    case "rideCymbals":
+      return rideCymbals;
+      default:
+        return;
+  }
+};
+
+const toggleDrum = (drumArrayName, index) => {
+    const drums = getDrumArrayByName(drumArrayName);
+     if(!drums || index > 15 || index < 0) {
+         return;
+     }
+     drums[index] = !drums[index];
+};
+
+const clear = (drumArrayName) => {
+    const drums = getDrumArrayByName(drumArrayName);
+    if (drums) {
+        drums.fill(false);
+    }
+}
+
+const invert = (drumArrayName) => {
+    const drums = getDrumArrayByName(drumArrayName);
+    if (!drums) {
+        return;
+    }
+    for (let i = 0; i < drums.length; i++) {
+        drums[i] = !drums[i];
+    }
+};
